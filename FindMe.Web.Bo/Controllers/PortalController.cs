24f26a10d349361de87cc91050f6ac67d2b13ca7
DAMIENPC\Damien <damient@swappdev.io>
2016-12-03 11:03:24 +0400

    Add project files.

---
+++ FindMe.Web.Bo/Controllers/PortalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindMe.Web.App.Controllers
+{
+    [Route("")]
+    [Route("[controller]")]
+    [Route("[controller]/[action]")]
+    public class PortalController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public PartialViewResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return PartialView();
+        }
+
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
