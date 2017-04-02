fec9c6cf0e0531a2f0cc7844e85a5bcda891f167
DAMIENPC\Damien <damient@swappdev.io>
2017-04-02 15:50:44 +0400

    miscs

--- FindMe.Web.Bo/Migrations/20170402114814_Mig_006.Designer.cs
+++ FindMe.Web.Bo/Migrations/20170402114814_Mig_006.Designer.cs
@@ -10,9 +10,10 @@ using FindMe.Data;
 namespace FindMe.Web.Bo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170402114814_Mig_006")]
+    partial class Mig_006
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
---
+++ FindMe.Web.Bo/Migrations/20170402114814_Mig_006.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FindMe.Web.Bo.Migrations
+{
+    public partial class Mig_006 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactCivility",
+                table: "SuggestAddresses",
+                maxLength: 32,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Civility",
+                table: "RegisterCompanys",
+                maxLength: 32,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactCivility",
+                table: "SuggestAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "Civility",
+                table: "RegisterCompanys");
+        }
+    }
+}
