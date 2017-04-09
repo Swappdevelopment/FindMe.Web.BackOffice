23a819db8b80fadc63dfb8f6206ef4f654c914aa
Huby <Huby@DESKTOP-H2PDN36>
2017-04-09 08:26:31 +0300

    import added

--- FindMe.Web.Bo/wwwroot/_references.js
+++ FindMe.Web.Bo/wwwroot/_references.js
@@ -1,9 +1,11 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/accentfold.js" />
 /// <reference path="js/accentfold.min.js" />
 /// <reference path="js/account/app-signin.js" />
 /// <reference path="js/account/signInCtrlr.js" />
 /// <reference path="js/app/account/profileCtrlr.js" />
 /// <reference path="js/app/admin/addressesCtrlr.js" />
+/// <reference path="js/app/admin/bulkimportctrlr.min.js" />
 /// <reference path="js/app/admin/categoriesctrlr.js" />
 /// <reference path="js/app/admin/citiesctrlr.js" />
 /// <reference path="js/app/admin/clientsCtrlr.js" />
@@ -13,5 +15,4 @@
 /// <reference path="js/app/homeCtrlr.js" />
 /// <reference path="js/app/index.js" />
 /// <reference path="js/app/mainCtrlr.js" />
-/// <reference path="js/site - copy.js" />
 /// <reference path="js/site.js" />
