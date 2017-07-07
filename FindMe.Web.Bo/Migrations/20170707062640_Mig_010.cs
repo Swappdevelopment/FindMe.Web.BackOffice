b26e0f458627dbea253f564b17a787d8ee382f4f
DAMIENPC\Damien <damient@swappdev.io>
2017-07-07 10:36:28 +0400

    miscs

---
+++ FindMe.Web.Bo/Migrations/20170707062640_Mig_010.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FindMe.Web.Bo.Migrations
+{
+    public partial class Mig_010 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "IPAddress_Id",
+                table: "Ratings",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "IPAddress_Id",
+                table: "Ratings",
+                nullable: true,
+                oldClrType: typeof(long));
+        }
+    }
+}
